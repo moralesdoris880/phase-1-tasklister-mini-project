2fa16512639b2d8bb173e0c54daaf4fca038c6ca
Doris Morales <moralesdoris880@gmail.com>
2021-11-08 12:50:55 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  
+  document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    handletodo(e.target.newtask.value)
+    
+  })
+})
+
+function handletodo(todo){
+  let p = document.createElement('p');
+  p.textContent = todo;
+  document.querySelector('#tasks').appendChild(p);
+}
